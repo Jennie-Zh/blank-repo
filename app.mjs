01333bdac02350cbc5051844f8eaf9ea91d675e0
Qian-Zhang68 <22198289@student.westernsydney.edu.au>
2026-08-13 18:13:39 +1000

    Add app.mjs demonstrating array-toolbox

---
+++ app.mjs
@@ -0,0 +1,8 @@
+import { sum, average, unique, chunk, intersection } from './array-toolbox.mjs';
+
+console.log('--- Array Toolbox Demo ---');
+console.log('Sum:', sum([1, 2, 3, 4]));
+console.log('Average:', average([2, 4, 6]));
+console.log('Unique:', unique([1, 1, 2, 3, 3]));
+console.log('Chunk:', chunk([1, 2, 3, 4, 5], 2));
+console.log('Intersection:', intersection([1, 2, 3], [2, 3, 4]));
